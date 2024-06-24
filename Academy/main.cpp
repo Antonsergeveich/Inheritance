1fc81838131c4df71793200890aaa4e02b75ae18
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-06-24 20:57:42 +0300

    Before ostream overload

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -251,8 +251,8 @@ void main()
 	cout << delimiter << endl;
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
-		group[i]->info();
-		//cout << *group[i] << endl;
+		//group[i]->info();
+		cout << *group[i] << endl;
 		cout << delimiter << endl;
 	}
 	
