fbb257a09369793d6562b0aab8e54ff453256246
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-08-18 18:39:03 +0300

    +

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -206,18 +206,23 @@ namespace Geometry // объявляем пространство имён Geome
 			//5) Выбираем чем и на чём рисовать:
 			SelectObject(hdc, hPen);
 			SelectObject(hdc, hBrush);
-
 			//6) Рисуем прямоугольник:
 			::Rectangle(hdc, start_x, start_y, start_x + width/*800*/,start_y + height /*350*/); 
 			//start_x, start_y - координаты верхнего левого угла
 			//800,350 - координаты нижнего правого угла.
-
 			//Двойное двоеточие :: без операнда слева означает что класс Rectangle берётся из глобального пространства имён;
 			//7)Освобождаем ресурсы:
 			DeleteObject(hPen);
+			//Функция DeleteObject удаляет логическое перо, кисть, шрифт, растровое изображение, 
+			//область или палитру, освобождая все системные ресурсы,связанные с объектом.
+			//После удаления объекта указанный дескриптор больше недействителен.
 			DeleteObject(hBrush);
-
 			ReleaseDC(hwnd, hdc);
+			//Функция ReleaseDC освобождает контекст устройства (DC),
+			//освобождая его для использования другими приложениями. 
+			//Влияние функции ReleaseDC зависит от типа контроллера домена.
+			//Он освобождает только общие контроллеры домена и контроллеры домена окон.
+			//Он не влияет на класс или частные контроллеры домена.
 			/*for (int i = 0; i < height; i++)
 			{
 				for (int j = 0; j < width; j++)
@@ -291,12 +296,20 @@ namespace Geometry // объявляем пространство имён Geome
 		void draw()const override
 		{
 			HWND hwnd = GetConsoleWindow();
+			//1)Возвращает дискриптор окна консоли. 
+			//description - переменная в которой хранится описание чего-то;
+			//HWND - Handler to Window (обработчик (Дискриптор) окна)
 			HDC hdc = GetDC(hwnd);
-
+			//2) Получаем контекст устройства (Device Context) окна консоли.
+			// DC - это то, на чём будем рисовать
 			HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+			//3) Создаём карандаш. pen рисует контур фигуры.
+			// PS_SOLID - сплошная линия
+			// 5 - толщина линии в пикселах
 			HBRUSH hBrush = CreateSolidBrush(color);
-
+			//4)Кисточка заливка
 			SelectObject(hdc, hPen);
+			//5) Выбираем чем и на чём рисовать:
 			SelectObject(hdc, hBrush);
 
 			::Ellipse(hdc, start_x, start_y, start_x + get_diameter(), start_y + get_diameter());
@@ -396,18 +409,18 @@ void main()
 	setlocale(LC_ALL, "");
 	//Square shape(Color::CONSOLE_BLUE);
 	//Geometry::Square square(5, Geometry::CONSOLE_RED);
-	Geometry::Square square(50, 200, 250, 5, Geometry::Color::BLUE);
+	//Geometry::Square square(50, 200, 250, 5, Geometry::Color::BLUE);
 	/*cout << "Длина стороны квадрата: " << square.get_side() << endl;
 	cout << "Площадь квадрата: " << square.get_area() << endl;
 	cout << "Периметр квадрата: " << square.get_perimeter() << endl;
 	square.draw();*/
-	square.info();
+	//square.info();
 
-	Geometry::Rectangle rect(150, 250, 400, 50, 3, Geometry:: BLUE);
-	rect.info();
+	/*Geometry::Rectangle rect(150, 250, 400, 50, 3, Geometry:: BLUE);
+	rect.info();*/
 
-	/*Geometry::Circle circle(75, 700, 50, 5, Geometry::Color::YELLOW);
-	circle.info();*/
+	Geometry::Circle circle(75, 800, 150, 5, Geometry::Color::YELLOW);
+	circle.info();
 
 	/*Geometry::Rectangle rect{ 150, 80, 500, 50, 3, Geometry::Color::GREEN};
 	rect.info();*/
